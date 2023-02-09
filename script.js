dc394c7c2a667bd5588f66376cf7281ac6718711
tod619 <trevorodwyer619@hotmail.com>
2023-02-09 21:38:06 +0000

    add transition effect on heading span 2 in script.js + style.css files

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const container = document.querySelector('.container')
+const headingSpan2 = document.querySelector('.heading-span-2')
 
 
 document.querySelector('.signup-btn').addEventListener('click', ()=>{
     container.classList.add('change')
+
+    setTimeout(()=>{
+        headingSpan2.textContent = 'Up'
+    }, 200)
+    
 })
 
 document.querySelector('.signin-btn').addEventListener('click', ()=> {
-    container.classList.remove('change')    
+    container.classList.remove('change')
+    setTimeout(()=>{
+        headingSpan2.textContent = 'In'
+    },200)
+        
 })
